5872fd783472cc90e0c3a1c71fffead496cfa97e
tientm <tientm@ahamove.com>
2020-02-27 00:17:02 +0700

    commit register & verify

--- docker-flask-mongo/web/restApi.py
+++ docker-flask-mongo/web/restApi.py
@@ -8,7 +8,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 import time
 import redis
-import bcrypt 
+import bcrypt
+
 # db
 from pymongo import MongoClient
 
@@ -19,18 +20,140 @@ clients = MongoClient("mongodb://db:27017")
 db = clients.SentencesDatabase
 users = db["Users"]
 
+
 class Register(Resource):
     def post(self):
         # Step1 is to get posted data by the user
         postedData = request.get_json()
-        
+
         # Get the data
         username = postedData["username"]
         password = postedData["password"]
-        
-        #hash(password + salt) = wdftdad4
-        
+
+        # hash(password + salt) = wdftdad4
+        hashed_pw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+        # Stote username and pw into the db
+        users.insert({
+            "Username": username,
+            "Password": password,
+            "Sentence": "",
+            "Tokens": 5
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "You succesfully signed up for the Api",
+            "hashpw": str(hashed_pw)
+        }
+        return jsonify(retJson)
+
+
+def verifyPw(username, password):
+    hashed_pw = users.find({
+        "Username": username,
+    })[0]["Password"]
     
+    if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
+        return True
+    else:
+        return False
+
+
+def countTokens(username):
+    tokens = users.find({
+        "Username": username
+    })[0]["Tokens"]
+
+
+class Store(Resource):
+    def post(self):
+        # Step 1  get the posted data
+        postedData = request.get_json()
+
+        # Step 2 is to read the data
+        username = postedData["username"]
+        password = postedData["password"]
+        sentence = postedData["sentence"]
+
+        # Step 3 verify the username pw match
+        correct_pw = verifyPw(username, password)
+        if not correct_pw:
+            retJson = {
+                "status": 302,
+                "msg": "Not correct pw"
+            }
+            return jsonify(retJson)
+        # Step 4 Verify user has enough tokens
+        num_tokens = countTokens(username)
+        if num_tokens <= 0:
+            retJson = {
+                "status": 301,
+                "msg": "Not enough tokens"
+            }
+            return jsonify(retJson)
+        # Step 5 store the sentence, take one token away and return 200 OK
+        users.update({
+            "Username": username
+        }, {
+            "$set": {"Sentence": sentence},
+            "Tokens": num_tokens - 1
+        })
+        retJson = {
+            "status": 200,
+            "msg": "Sentence saved successfully"
+        }
+        return jsonify(retJson)
+
+
+class Verify(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        # Step 3 verify username pw mathch
+        correct_pw = verifyPw(username, password)
+        
+        if not correct_pw:
+            retJson = {
+                "status": 302
+            }
+            return jsonify(retJson)
+        # Step 4 Verify user has enough tokens
+        # num_tokens = countTokens(username)
+        # if num_tokens <= 0:
+        #     retJson = {
+        #         "status": 301,
+        #         "msg": "Not enough tokens"
+        #     }
+        #     return jsonify(retJson)
+
+        sentence = users.find({
+            "Username": username
+        })[0]["Sentence"]
+
+        retJson = {
+            "status": 200,
+            "sentence": sentence
+        }
+        return jsonify(retJson)
+
+
+# Return api
+api.add_resource(Register, '/register')
+api.add_resource(Store, '/store')
+api.add_resource(Verify, '/verify')
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
 
 """
 from flask import Flask, jsonify, request
